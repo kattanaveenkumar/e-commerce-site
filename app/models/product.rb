45b60ca80309a466a972e3ba14c0709240ef0ec6
kattanaveenkumar <katta.naveekumar95@gmail.com>
2020-04-21 16:01:41 +0530

    syntax issue

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,11 @@ class Product < ApplicationRecord
 	has_many :product_categories, dependent: :destroy
 	has_many :categories, through: :product_categories
 
-	validates :name, :image, :descrition
-	
-	has_attached_file :image
+	validates :name, :image, :description
+
+	has_attached_file :image,
+										:url => "/attachments/:id/:style/:basename.:extension",
+                    :path => ":rails_root/public/products/attachments/:id/:style/:basename.:extension"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
