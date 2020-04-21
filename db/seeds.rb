ab997a377c513ba315d46b99aa967573c4372f23
kattanaveenkumar <katta.naveekumar95@gmail.com>
2020-04-21 14:23:57 +0530

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create(email: "admin@carmatec.com", password: "admin@123", password_confirmation: "admin@123")
+User.create(email: "user@carmatec.com", password: "user@123"), password_confirmation: "user@123")
